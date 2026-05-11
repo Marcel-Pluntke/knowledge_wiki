20946f8df385f4b3551f6a6846cb77d69f1f8fc6
Marcel Pluntke <111451037+Marcel-Pluntke@users.noreply.github.com>
2026-05-12 00:45:12 +0200

    Show WinCC OA CTRL page in sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -1,7 +1,19 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-  tutorialSidebar: ['intro', 'documentation', 'automation', 'test-engineering', 'measurement-technology', 'more-topics'],
+  tutorialSidebar: [
+    'intro',
+    'documentation',
+    'automation',
+    'test-engineering',
+    'measurement-technology',
+    {
+      type: 'category',
+      label: 'WinCC OA',
+      items: ['wincc-oa/wincc-oa-ctrl-links'],
+    },
+    'more-topics',
+  ],
 };
 
 export default sidebars;
