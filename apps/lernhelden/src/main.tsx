3b863bfb5b6e68e5f90cb4f5a805937d233e21a9
Marcel Pluntke <111451037+Marcel-Pluntke@users.noreply.github.com>
2026-08-19 20:14:46 +0200

    Mount football motion enhancer

--- apps/lernhelden/src/main.tsx
+++ apps/lernhelden/src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {App} from './App';
 import {FootballExtension} from './components/FootballExtension';
+import {FootballMotionEnhancer} from './components/FootballMotionEnhancer';
 import './styles.css';
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App/><FootballExtension/></StrictMode>);
+createRoot(document.getElementById('root')!).render(<StrictMode><App/><FootballExtension/><FootballMotionEnhancer/></StrictMode>);
